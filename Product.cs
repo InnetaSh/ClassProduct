f7ced09200b6862447a6c7f7a1d4805f3921550f
InnetaSh <inneta3@gmail.com>
2024-06-27 11:30:07 +0300

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,75 @@
+﻿using ClassProduct;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+//Создайте класс Product, который будет представлять продукт с следующими полями:
+
+//Id(тип int)
+//Name(тип string)
+//Price(тип decimal)
+//Category(тип string)
+//Реализуйте интерфейс IComparable<Product> в классе Product. Напишите метод CompareTo, который будет сравнивать продукты по их цене. Если цены одинаковые, сравните по имени продукта.
+
+//Создайте класс ProductComparerByName для сортировки продуктов по имени. Реализуйте в этом классе интерфейс IComparer<Product>.
+
+namespace ClassProduct
+{
+    internal class Product : IComparable<Product>
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+        public string Category;
+        public Product(int productId, string name, decimal price, string category)
+        {
+            Id = productId;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public int CompareTo(Product? product)
+        {
+            if (product is null) throw new ArgumentException("Некорректное значение параметра");
+            int rez = (int)(Price - product.Price);
+            if (rez != 0)
+                return rez;
+            else
+                return Name.CompareTo(product.Name);
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Id: {Id}, name - {Name}, price - {Price}, category - {Category}");
+        }
+    }
+}
+
+class ProductComparerByName : IComparer<Product>
+{
+
+    public int Compare(Product? x, Product? y)
+    {
+        if (x is null || y is null) throw new ArgumentException("Некорректное значение параметра");
+        return x.Name.CompareTo(y.Name);
+    }
+    
+}
+
+class ProductComparerByCategory : IComparer<Product>
+{
+    public int Compare(Product? x, Product? y)
+    {
+        if (x is null || y is null) throw new ArgumentException("Некорректное значение параметра");
+        int rez = x.Category.CompareTo(y.Category);
+        if (rez != 0)
+            return rez;
+        return (int)(x.Price - y.Price);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿//Создайте класс Product, который будет представлять продукт с следующими полями:
+
+//Id(тип int)
+//Name(тип string)
+//Price(тип decimal)
+//Category(тип string)
+//Реализуйте интерфейс IComparable<Product> в классе Product. Напишите метод CompareTo, который будет сравнивать продукты по их цене. Если цены одинаковые, сравните по имени продукта.
+
+//Создайте класс ProductComparerByName для сортировки продуктов по имени. Реализуйте в этом классе интерфейс IComparer<Product>.
+
+//Создайте класс ProductComparerByCategory для сортировки продуктов по категории и по цене внутри категории. Реализуйте в этом классе интерфейс IComparer<Product>.
+
+//Создайте коллекцию из нескольких объектов Product.
+
+//Реализуйте метод для сортировки коллекции с использованием метода Sort класса List<T> с различными компараторами.
+
+//Выведите отсортированные коллекции на экран.
+
+
+
+//Требования:
+//Реализовать класс Product с описанными полями и методом CompareTo.
+//Реализовать классы ProductComparerByName и ProductComparerByCategory.
+//Создать метод для заполнения коллекции объектов Product.
+//Написать метод для сортировки и вывода коллекции на экран с использованием различных компараторов.
+
+using ClassProduct;
+using System.Collections.Generic;
+
+List<Product> products = new List<Product>
+        {
+            new Product (1, "яблоко", 59.99m, "фрукт"),
+            new Product (2,"груша", 49.99m,  "фрукт" ),
+            new Product( 3, "персик", 119.99m,  "фрукт" ),
+            new Product (4, "диван", 17999.99m,  "мебель"),
+            new Product (5,  "шкаф",  1499.99m, "мебель" ),
+            new Product( 6, "нектарин", 119.99m,  "фрукт" ),
+            new Product (7,  "стул",  1499.99m, "мебель" ),
+            new Product (8, "вишня", 20.99m, "фрукт")
+        };
+Console.WriteLine("Список продуктов до сортировки:");
+PrintProducts(products);
+Console.WriteLine();
+
+products.Sort();
+Console.WriteLine("\nСписок продуктов после сортировки по цене, по имени:");
+PrintProducts(products);
+
+products.Sort(new ProductComparerByName());
+Console.WriteLine("\nСписок продуктов после сортировки по имени:");
+PrintProducts(products);
+
+products.Sort(new ProductComparerByCategory());
+Console.WriteLine("\nСписок продуктов после сортировки по категории, по цене:");
+PrintProducts(products);
+
+
+void PrintProducts(List<Product> products)
+{
+    foreach (var pr in products)
+    {
+        pr.PrintInfo();
+    }
+}
